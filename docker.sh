674564a9c1fe45dc7a5d60b9c7b57b4035ca1eac
DangNguyen146 <19521317@gm.uit.edu.vn>
2022-08-26 12:38:43 +0700

    auto run vnutour

---
+++ docker.sh
@@ -0,0 +1,8 @@
+cp .env.example .env
+docker-compose up -d
+docker-compose exec app composer update --no-scripts
+docker-compose exec app composer dump-autoload
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan config:cache
+docker-compose exec app composer install
+docker-compose exec app php artisan migrate
