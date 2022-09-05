b0c0eb471ac445986018bb4e2fd0879a6ae1f1dd
dangnguyen146 <19521317@gm.uit.edu.vn>
2022-09-05 18:09:31 +0000

    fix errro permission

--- docker.sh
+++ docker.sh
@@ -6,3 +6,6 @@ docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan config:cache
 docker-compose exec app composer install
 docker-compose exec app php artisan migrate
+
+# cd /var/www/html
+docker-composer exec app chown -R www-data storage
